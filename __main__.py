1e0ef44e6a52896856419e7110242dea554fb2fd
TimHasert <timhasert@gmail.com>
2021-06-30 19:37:00 +0200

    script implementation

--- __main__.py
+++ __main__.py
@@ -5,17 +5,40 @@ Script entrypoint
 """
 
 from sys import argv
-from typing import Tuple
-from PyPDF4 import PdfFileMerger, PdfFileReader, PdfFileWriter
+from PyPDF4 import PdfFileReader, PdfFileWriter
+from argparse import ArgumentParser, Namespace
 
 
-def split(file: PdfFileReader) -> Tuple([PdfFileWriter, PdfFileWriter]):
-    count : int = file.getNumPages()
-    pivot : int =  
-    
-
-print(argv)
-
-p = PdfFileReader(argv[1])
-
-print(p.getNumPages())
+# argparse
+parser : ArgumentParser = ArgumentParser(
+    description='Shuffle PDF pages, taking the first and last page each until reaching the middle of the document',
+    allow_abbrev=True    
+)
+parser.add_argument(
+    'path',
+    type=str,
+    help='path to source file'
+)
+parser.add_argument(
+    '-o', '--out',
+    type=str,
+    help='path to output file (default: same as input file)'
+)
+args : Namespace = parser.parse_args()
+# init vars
+src : PdfFileReader = PdfFileReader(args.path)
+out : PdfFileWriter = PdfFileWriter()
+i : int = 0
+j : int = src.getNumPages()-1
+# main loop
+while True:
+    if j < i: break
+    out.addPage(src.getPage(i))
+    if j == i: break
+    out.addPage(src.getPage(j))
+    i += 1
+    j -= 1
+# output
+p : str = args.out if args.out is not None else args.path
+with open(p, 'wb') as f:
+    out.write(f)
